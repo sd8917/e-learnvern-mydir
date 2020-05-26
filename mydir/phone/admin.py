60805da7e8f94cc9c0bec3d7a8f6eab182995a4c
sd8917 <sudhanshuraj8917@gmail.com>
2020-05-26 14:36:55 +0530

    added import-export in sixth_branch

--- mydir/phone/admin.py
+++ mydir/phone/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
 from . models import Phone #Model_name
 
-admin.site.register(Phone)
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+
+# Register your models here.
+class PhoneResource(resources.ModelResource):
+
+    class Meta:
+        model = Phone
+        exclude = ('id', )
+
+class PhoneAdmin(ImportExportModelAdmin):
+    resource_class = PhoneResource
+
+admin.site.register(Phone,PhoneAdmin)
+
+# admin.site.register(Phone)
+
