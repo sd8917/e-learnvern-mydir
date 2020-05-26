61ee18b4242c577c4d4ac3bd57c839467ad6c41b
sd8917 <sudhanshuraj8917@gmail.com>
2020-05-26 10:40:16 +0530

    show button functionality added fourth branch

--- mydir/phone/models.py
+++ mydir/phone/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Phone(models.Model):
+    name = models.CharField(max_length=55)
+    number = models.BigIntegerField()
+
+    def __str__(self):
+        return self.name + '  -   ' + str(self.number)
--- mydir/phone/views.py
+++ mydir/phone/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Phone
 
 # Create your views here.
 
 def home(request):
     return render(request, 'phone/home.html')
+
+
+def show(request):
+    names = Phone.objects.all()
+
+    return render(request, 'phone/show.html', {'names': names})
