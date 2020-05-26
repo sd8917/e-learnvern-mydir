adb1d7d2b31ad6c42cd5cab1a674d64ca3a86e55
sd8917 <sudhanshuraj8917@gmail.com>
2020-05-26 09:49:08 +0530

    front design and third branch

--- mydir/phone/views.py
+++ mydir/phone/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>This is the home page</h1>')
+    return render(request, 'phone/home.html')
