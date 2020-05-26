e0756fb8958fdb09e72067a2292d856983c31302
sd8917 <sudhanshuraj8917@gmail.com>
2020-05-26 11:18:14 +0530

    final commit adding data to database

--- mydir/phone/views.py
+++ mydir/phone/views.py
@@ -5,8 +5,20 @@ from . models import Phone
 # Create your views here.
 
 def home(request):
-    return render(request, 'phone/home.html')
+    #if we make a post request means we are going to add something to the data base then 
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        number = request.POST.get('number')
+        print(name)
+        print(number)
 
+        p = Phone(name=name, number=number)
+        p.save()
+
+        return render(request, 'phone/home.html', {'message': 'Your phone number is added'})
+
+    else:
+        return render(request, 'phone/home.html')
 
 def show(request):
     names = Phone.objects.all()
